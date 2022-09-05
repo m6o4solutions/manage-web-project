a523d87b9a375d374b21dd40f64d2ee7e4591f71
Michael Owuor <michael.owuor@gmail.com>
2022-09-05 17:11:41 +0300

    feat: hero section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,11 @@ module.exports = {
 			ubuntu: 'Ubuntu, sans-serif',
 		},
 		screens: {
-			sm: '480px',
+			sm: '640px',
 			md: '768px',
-			lg: '976px',
-			xl: '1440px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
 		},
 		extend: {
 			colors: {
