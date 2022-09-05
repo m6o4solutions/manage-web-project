ff8fbbdf58d30b0232249284a514aebe2d2b9997
Michael Owuor <michael.owuor@gmail.com>
2022-09-05 15:24:59 +0300

    chore: tailwind customisation and configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,24 @@ module.exports = {
 		fontFamily: {
 			ubuntu: 'Ubuntu, sans-serif',
 		},
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				brightOrange: 'hsl(12, 88%, 59%)',
+				brightOrangeLight: 'hsl(12, 88%, 69%)',
+				brightOrangeSuperLight: 'hsl(12, 88%, 95%)',
+				darkBlue: 'hsl(228, 39%, 23%)',
+				darkBlueGray: 'hsl(227, 12%, 61%)',
+				veryDarkBlue: 'hsl(233, 12%, 13%)',
+				veryPaleOrange: 'hsl(13, 100%, 96%)',
+				veryLightGray: 'hsl(0, 0%, 98%)',
+			},
+		},
 	},
 	plugins: [require('@tailwindcss/forms')],
 };
